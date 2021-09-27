321eec339fda7aa8690206834ae31aacf5c63c4f
hyunlee214 <hyun110214@naver.com>
2021-09-27 21:06:26 +0900

    스크롤, 닉네임중복 기능추가

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 const path = require('path');
 const server = http.createServer(app);
 const socketIO = require('socket.io');
+//스크롤 추가
+const moment = require('moment');
 
 // io변수를 통해 메세지 제어
 const io = socketIO(server);
@@ -16,7 +18,12 @@ io.on('connection', (socket) => {
   // 서버로 보낸 내용 출력
   socket.on('chatting', (data) => { 
     // 서버에서 보낼 내용 
-    io.emit('chatting', data)
+    const {name, msg} = data;
+    io.emit('chatting', {
+      name,
+      msg,
+      time: moment(new Date()).format("h:ss A")
+    })
   })
 })
 
--- src/js/chat.js
+++ src/js/chat.js
@@ -6,6 +6,7 @@ const nickname = document.querySelector('#nickname');
 const chatList = document.querySelector('.chatting-list');
 const chatInput = document.querySelector('.chatting-input');
 const sendButton = document.querySelector('.send-button');
+const displayScroll = document.querySelector('.display-Container');
 
 // 이벤트 생성
 sendButton.addEventListener('click', () => {
@@ -19,10 +20,12 @@ sendButton.addEventListener('click', () => {
 // 서버에서 보낸 메세지 받는 기능 + 출력
 // 서버에서 데이터를 받을 때마다 li데이터 호출
 socket.on('chatting', (data) => {
+  console.log(data);
   const { name, msg, time } = data;
   const item = new LiModel(name, msg, time);
   // 데이터 넘겨받으면, makeLi()메서드 호출
-  item.makeLi()
+  item.makeLi() 
+  displayScroll.scrollTo(0, displayScroll.scrollHeight)
 }) 
 
 function LiModel(name, msg, time) {
@@ -32,7 +35,10 @@ function LiModel(name, msg, time) {
 
   this.makeLi = () => {
     const li = document.createElement("li");
-  
+    
+    // 설정한 nickname값이 서버에서 받은 이름과 같으면 'sent' 아니면, 'received'
+    li.classList.add(nickname.value === this.name ? "sent" : "received")
+    
     const dom = `<span class="profile">
     <span class="user">${this.name}</span>
     <img class="image" src="https://placeimg.com/50/50/any" alt="any">
