cdb8b2a97441e2143638d42c4e25f73e289b8f6c
hyunlee214 <hyun110214@naver.com>
2021-09-27 15:57:26 +0900

    서버로 메세지 전송테스트 [오브젝트 형태]

--- src/js/chat.js
+++ src/js/chat.js
@@ -2,6 +2,22 @@
 
 const socket = io();
 
+const nickname = document.querySelector('#nickname');
+const chatList = document.querySelector('.chatting-list');
+const chatInput = document.querySelector('.chatting-input');
+const sendButton = document.querySelector('.send-button');
+
+// 이벤트 생성
+sendButton.addEventListener('click', () => {
+  const param = {
+    name : nickname.value,
+    msg : chatInput.value
+  }
+
+  socket.emit('chatting', param);
+})
+
+
 // socket 객체를 이용하여 메세지 송신
 // 'chatting'이라는 채널 ID를 적어서 송신 / 내용 : 문자열
 socket.emit('chatting', 'from client');
